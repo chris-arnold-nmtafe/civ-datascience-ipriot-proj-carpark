5a724baa40bcf3b3b0ebc42472a34c58b5b6dc64
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2025-08-06 17:10:08 +0800

    moved UI stub code

---
+++ smartpark/mocks.py
@@ -0,0 +1,25 @@
+from interfaces import CarparkSensorListener
+from interfaces import CarparkDataProvider
+import time
+
+class MockCarparkManager(CarparkSensorListener,CarparkDataProvider):
+    @property
+    def available_spaces(self):
+        return 1000
+
+    @property
+    def temperature(self):
+        return 1000
+
+    @property
+    def current_time(self):
+        return time.localtime()
+
+    def incoming_car(self,license_plate):
+        print('Car in! ' + license_plate)
+
+    def outgoing_car(self,license_plate):
+        print('Car out! ' + license_plate)
+
+    def temperature_reading(self,reading):
+        print(f'temperature is {reading}')
